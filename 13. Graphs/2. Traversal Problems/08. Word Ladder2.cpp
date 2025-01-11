6debde4732069f886068330a8311aa0d80e9750f
Anukul Dewangan <109734962+Anu-22cool@users.noreply.github.com>
2025-01-11 12:32:40 +0530

    Update 08. Word Ladder2.cpp

--- 13. Graphs/2. Traversal Problems/08. Word Ladder2.cpp
+++ 13. Graphs/2. Traversal Problems/08. Word Ladder2.cpp
@@ -38,5 +38,69 @@ The sum of all shortest transformation sequences does not exceed 105.
 Solution:
 
 
+class Solution {
+public:
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+        
+          unordered_map<string,int>mp;
+          for(int i=0;i<wordList.size();i++)
+           mp[wordList[i]]++;
+          queue<vector<string>>q;
+          vector<vector<string>>ans;
+          q.push({beginWord}); 
+          int level=0;
+          int minlevel=0;
+          while(!q.empty())
+          {
+                int size=q.size();
+                set<string>used;
+                level++;
+                for(int i=0;i<size;i++)
+                {
+                    vector<string>temp=q.front();
+                    q.pop();
+                    string s=temp.back();
+                    for(int i=0;i<s.size();i++)
+                    {   char c=s[i];
+                        for(char ch='a';ch<'z';ch++)
+                        {
+                            if(ch!=c)
+                            {
+                                s[i]=ch;
+                                if(mp.find(s)!=mp.end()&& mp[s]!=0)
+                                {
+                                    used.insert(s);
+                                    temp.push_back(s);
+                                    q.push(temp);
+                                    if(s==endWord)
+                                    {
+                                        ans.push_back(temp);
+                                    }
+                                    temp.pop_back();
+                                    
+                                     
+                                }
+                            }
+                        }
+                        s[i]=c;
+                    }
+                }
+
+                for(auto it:used)
+                {
+                    mp[it]=0;
+                }
+                if(ans.size()!=0)
+                 return ans;
+             
+          } 
+
+          
+          
+          return ans; 
+
+    }
+};
+
 
 
